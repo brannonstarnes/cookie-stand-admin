5581b67c25dab0e2c796b1df42bd7823c7477c9a
Brannon Starnes <brannlee808@gmail.com>
2022-01-20 18:09:11 -0800

    completes features

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <>
-      <Header />
-      <Form />
-    </>
+    <div>
+      <Head>
+        <title>Cookie Stand Admin</title>
+      </Head>
+
+      <body className="bg-green-50 h-screen w-screen">
+        <Header />
+        <Form />
+        <Message />
+        <Footer />
+      </body>
+    </div>
   );
 }
 
@@ -18,28 +28,28 @@ function Header() {
 function Form() {
   return (
     <form>
-      <div className="h-36 w-2/3 bg-green-400 mx-auto mt-6">
+      <div className=" h-36 w-5/6 bg-green-400 mx-auto mt-6 rounded">
         <h2 className="text-center pt-2 font-bold">Create Cookie Stand</h2>
 
         <label className="pl-4 pr-2 w-full">
           Location
-          <input type="text" className="w-5/6 m-2" />
+          <input type="text" className="w-5/6 m-2"></input>
         </label>
 
         <div className="flex flex-row pl-4 pr-4">
           <div className="w-1/4">
             <p className="text-center">Minimum Customers per Hour</p>
-            <input type="integer" className="w-full" />
+            <input type="integer" className="w-full"></input>
           </div>
 
           <div className="pl-4 w-1/4">
             <p className="text-center">Maximum Customers per Hour</p>
-            <input type="integer" className="w-full" />
+            <input type="integer" className="w-full"></input>
           </div>
 
           <div className="pl-4 w-1/4">
             <p className="text-center">Average Cookies per Sale</p>
-            <input type="integer" className="w-full" />
+            <input type="integer" className="w-full"></input>
           </div>
 
           <div className="pl-4 w-1/4">
@@ -48,5 +58,26 @@ function Form() {
         </div>
       </div>
     </form>
+  );
+}
+
+function Message() {
+  return (
+    <div className="text-center m-7">
+      <p className="pb-4 ">Report Table Coming Soon...</p>
+      <p>
+        {
+          '{"location":"Barcelona", "minCustomers":2,"maxCustomers":4,"avgCookies":2.5}'
+        }
+      </p>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="h-12 w-full bg-green-600 relative bottom-0 ">
+      <p className="h-6 p-3 font-bold text-sm">@2022</p>
+    </footer>
   );
 }
